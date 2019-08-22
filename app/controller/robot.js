50658b01a4f88c3e84cb32dc73edd7815036e082
zhouyg <yunge.zyg@alibaba-inc.com>
2019-08-22 10:39:23 +0800

    !!

--- app/controller/robot.js
+++ app/controller/robot.js
@@ -9,7 +9,7 @@ class Robot extends Controller {
     console.log(this.ctx.request.body);
 
     // const c = JSON.stringify(this.ctx.query) + '  ,  ' + JSON.stringify(this.ctx.request.body);
-    const c = this.ctx.request.body.text.content + ' !';
+    const c = this.ctx.request.body.text.content + '!!';
 
     request({
       url: apiUrl2,
